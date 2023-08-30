e32242ef4bb94247d8f409313c3576f2fdabf9e9
Emmanuel Solis <emma.ruso@outlook.com>
2023-08-30 10:49:49 -0600

    añadimos borde a los campos no validados y mensaje de error al formulario no completado

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-   /* Verificamos si es un numero   */
-   const numRegx = /^[0-9]+$/;
+/* Verificamos si es un numero   */
+const numRegx = /^[0-9]+$/;
 
-   
+/*
+   console.log("Aqui ando")
    function validar() {
-       console.log("Aqui ando")
    let nombre = document.querySelector('input[name="nombre"]').value;
    let precioInput = document.getElementById("precio");
    let precioValor = precioInput.value.trim() /* Eliminamos los espacios */
+/*
    let precio = document.querySelector('input[name="precio"]').value;
    if(!numRegx.test(precioValor)){
      alert("El precio debe ser un numero")
@@ -21,14 +22,46 @@
    } else {
        alert("Datos enviados correctamente");
    }
- }
- const d = document;
- const colorValidados = (idFormulario) => {
-    const listaCampos = d.querySelectorAll(`#${idFormulario} [data-validate]` );
-       console.log(listaCampos)
-    }
+ } 
+ */
+const d = document;
+const validarCampos = (idFormulario) => {
+  let listaCampos = d.querySelectorAll(`#${idFormulario} [data-validate]`);
+  let validacion = true;
+  if (listaCampos.length > 0) {
+    listaCampos.forEach((elemento) => {
+      const tipoElemento = elemento.getAttribute("type");
+      if (elemento.value === "") {
+        validacion = false;
+        elemento.style.setProperty("border", "1px solid red");
+        setTimeout(() => {
+          elemento.style.setProperty("border", "");
+        }, 2000);
+      }
 
- const enviarFormulario = () => {
-    event.preventDefault();
-    colorValidados('formValidacion')
- }
+      if (tipoElemento === "checkbox" && !elemento.checked) {
+        validacion = false;
+        elemento.style.setProperty("outline", "1px solid red");
+        setTimeout(() => {
+          elemento.style.setProperty("outline", "");
+        }, 2000);
+      }
+    });
+  }
+   return validacion;
+};
+
+const enviarFormulario = () => {
+  event.preventDefault();
+  if (!validarCampos('formValidacion')) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Algo salió mal !",
+      footer: "<h3>Verifique que no falte algún campo</h3>",
+    });
+    return;
+  } else {
+    alert("Datos enviados correctamente");
+  }
+};
